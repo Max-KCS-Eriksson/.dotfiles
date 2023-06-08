661e2a14599e48e63068f38138c211dae0eef65a
Max <teratoscincus@hotmail.com>
2023-06-09 00:04:21 +0200

    Shell: Clean up aliases and add new alias
    Added alias to cd to git repo root dir.

--- .config/shell/common/interactive/aliases.sh
+++ .config/shell/common/interactive/aliases.sh
@@ -4,29 +4,31 @@
 #   sleep 10; alert
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
 
-# Add color to basic commands.
+# Add default flags to commands.
+alias mv="mv -iv"
+alias rm="rm -iv"
+alias cp="cp -iv"
 alias ls="ls -F --color=auto"
 alias dir="dir --color=auto"
 alias vdir="vdir --color=auto"
 alias grep="grep --color=auto"
 alias fgrep="fgrep --color=auto"
 alias egrep="egrep --color=auto"
+alias tree="tree --dirsfirst -F -I '.git/|.gitignore|*_env/|__pycache__/|*.egg-info/|build/'"
 
-# Bash command aliases.
+# Quality of life command aliases.
 alias poweroff="sudo poweroff"
 alias reboot="sudo reboot"
 
-alias csb="clear; source $HOME/.config/shell/common/extras/greeter.sh"
-
 alias ..="cd .."
-alias c="clear"
+alias cdgr='cd $(git rev-parse --show-toplevel)'
+
 alias ll="ls -AlFh"
 alias la="ls -AF"
 alias l="ls -CF"
+
+alias c="clear"
 alias cll="clear; ll"
-alias mv="mv -iv"
-alias rm="rm -iv"
-alias cp="cp -iv"
 
 alias gr="grep"
 
@@ -34,9 +36,7 @@ alias v="nvim"
 alias v.="nvim ."
 
 alias fzp="fzf --preview 'bat --color=always {}'"
-
 alias xcc="xclip -selection clipboard"
-alias tree="tree --dirsfirst -F -I '.git/|.gitignore|*_env/|__pycache__/|*.egg-info/|build/'"
 
 # Terminal
 alias another='xfce4-terminal --working-directory=$PWD'
@@ -49,4 +49,5 @@ alias dot="/usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME"
 alias rice="/usr/bin/git --git-dir=$HOME/.rice --work-tree=$HOME"
 
 # Scripts.
+alias csb="clear; source $HOME/.config/shell/common/extras/greeter.sh"
 alias rp="python3 $HOME/repos/projects/reading_progress/reading_progress.py"
