eac8072f21bcb213b22bffe294afd55b5896513d
Max <teratoscincus@hotmail.com>
2023-06-12 14:58:47 +0200

    Zsh: Run commands specified in .auto after cd

--- .config/shell/zsh/util/hooks/cd_hook.zsh
+++ .config/shell/zsh/util/hooks/cd_hook.zsh
@@ -3,9 +3,14 @@
 _cd_hook() {
     if [[ "$(pwd)" != "$HOME" ]]; then
         ll
+
+        # Allow running of auto commands per dir
+        if [[ -f ./.auto ]]; then
+            echo -e '\nRunning auto commands'
+            source ./.auto
+        fi
     fi
 }
 
 autoload -U add-zsh-hook
 add-zsh-hook chpwd _cd_hook
-
