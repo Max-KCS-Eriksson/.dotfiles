df8edde78e4bf55481693e6b5367b764560eb065
Max <teratoscincus@hotmail.com>
2023-08-06 14:40:13 +0200

    Remove dirs from PATH as per rtx migration
    Go, Rust/cargo, Python, and Java are handled by rtx.

--- .config/shell/common/login/paths.sh
+++ .config/shell/common/login/paths.sh
@@ -1,14 +1,2 @@
-# Add the Golang binary
-PATH=$PATH:/usr/local/go/bin
-
-# Add Cargo binaries
-PATH=$PATH:"$HOME/.cargo/bin"
-
-# Pyenv
-PATH="$PYENV_ROOT/bin:$PATH"
-
-# Jenv
-PATH="$HOME/.jenv/bin:$PATH"
-
 # Add scripts folder
 PATH=$PATH:"$HOME/scripts"
