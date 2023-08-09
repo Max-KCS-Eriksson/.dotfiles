cb310646b9688f30189d77dc8337ffd5fa360969
Max <teratoscincus@hotmail.com>
2023-08-09 12:59:36 +0200

    Remove unused env vars per rtx migration

--- .config/shell/common/login/env_vars.sh
+++ .config/shell/common/login/env_vars.sh
@@ -1,15 +1,10 @@
 export EDITOR="/usr/bin/nvim"
 
+# XDG directories
 export XDG_CONFIG_HOME="$HOME"/.config
 export XDG_CACHE_HOME="$HOME"/.cache
 export XDG_DATA_HOME="$HOME"/.local/share
 export XDG_STATE_HOME="$HOME"/.local/state
-
-# Pyenv
-export PYENV_ROOT="$HOME/.pyenv"
-
-# NVM
-export NVM_DIR="$HOME/.nvm"
 
 # Bat
 export BAT_THEME="gruvbox-dark"
