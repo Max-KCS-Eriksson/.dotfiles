16c23550d9593707ae4dc8c56f384ce420ca5429
Max <teratoscincus@hotmail.com>
2023-08-10 15:29:13 +0200

    Shell: clean home with aliases

--- .config/shell/common/interactive/aliases.sh
+++ .config/shell/common/interactive/aliases.sh
@@ -1,29 +1,42 @@
 #!/usr/bin/env bash
 
-# Add an "alert" alias for long running commands.  Use like so:
-#   sleep 10; alert
-alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
+# Shadow commands
 
-# Add default flags to commands.
+# Ask before overwrite/delete
 alias mv="mv -iv"
 alias rm="rm -iv"
 alias cp="cp -iv"
+
+# Color output
 alias ls="ls -F --color=auto"
 alias dir="dir --color=auto"
 alias vdir="vdir --color=auto"
 alias grep="grep --color=auto"
 alias fgrep="fgrep --color=auto"
 alias egrep="egrep --color=auto"
-alias tree="tree --dirsfirst -F -I '.git/|.gitignore|*_env/|__pycache__/|*.egg-info/|build/'"
+
+# Ignore files in output
+alias tree="tree --dirsfirst -F -I '.git/|.gitignore|*_env/|.venv/|__pycache__/|*.egg-info/|build/'"
+
+# Clean home
+alias wget=wget --hsts-file='$XDG_DATA_HOME/wget-hsts'
+alias mvn='mvn -gs $XDG_CONFIG_HOME/maven/settings.xml'
 
 # Hide from history
 alias mpv=" mpv"
 alias feh=" feh"
 
-# Quality of life command aliases.
+# Ease of use
 alias poweroff="sudo poweroff"
 alias reboot="sudo reboot"
 
+# Shorthand aliases
+
+# Add an "alert" alias for long running commands.  Use like so:
+#   sleep 10; alert
+alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
+
+# Move up dir tree
 dots=".."
 target="cd .."
 for ((i = 0; i < 10; i++)); do
@@ -33,20 +46,24 @@ for ((i = 0; i < 10; i++)); do
 done
 unset dots target
 
+# Change dir to git root
 alias cdgr='cd $(git rev-parse --show-toplevel)'
 
+# List dir contents
 alias ll="ls -AlFh"
 alias la="ls -AF"
 alias l="ls -CF"
 
+# Clear
 alias c="clear"
 alias cll="clear; ll"
 
-alias gr="grep"
-
+# Text editing
 alias v="nvim"
 alias v.="nvim ."
 
+# Search, filter, and copy output
+alias gr="grep"
 alias fzp="fzf --preview 'bat --color=always {}'"
 alias xcc="xclip -selection clipboard"
 
