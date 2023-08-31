3a4460d271e2adc64b75db5fa71b454cedaf51da
Max <teratoscincus@hotmail.com>
2023-08-31 09:30:45 +0200

    Qtile: Refactor and reduce nesting in window funcs

--- .config/qtile/settings/keys.py
+++ .config/qtile/settings/keys.py
@@ -19,36 +19,51 @@ SPACE = "space"
 ESC = "Escape"
 
 
+def _window_to_group(qtile, direction=None):
+    if qtile.currentWindow or direction is None:
+        return
+
+    i = qtile.groups.index(qtile.currentGroup)
+    destination = i + direction
+    qtile.currentWindow.togroup(qtile.groups[destination].name)
+
+
 @lazy.function
 def window_to_prev_group(qtile):
-    if qtile.currentWindow is not None:
-        i = qtile.groups.index(qtile.currentGroup)
-        qtile.currentWindow.togroup(qtile.groups[i - 1].name)
+    direction = -1
+    _window_to_group(qtile, direction)
 
 
 @lazy.function
 def window_to_next_group(qtile):
-    if qtile.currentWindow is not None:
-        i = qtile.groups.index(qtile.currentGroup)
-        qtile.currentWindow.togroup(qtile.groups[i + 1].name)
+    direction = 1
+    _window_to_group(qtile, direction)
+
+
+def _window_to_screen(qtile, destination=None, switch_group=False, switch_screen=False):
+    if destination is None:
+        return
+
+    group = qtile.screens[destination].group.name
+    qtile.current_window.togroup(group, switch_group=switch_group)
+    if switch_screen:
+        qtile.cmd_to_screen(destination)
 
 
 def window_to_previous_screen(qtile, switch_group=False, switch_screen=False):
     i = qtile.screens.index(qtile.current_screen)
-    if i != 0:
-        group = qtile.screens[i - 1].group.name
-        qtile.current_window.togroup(group, switch_group=switch_group)
-        if switch_screen:
-            qtile.cmd_to_screen(i - 1)
+    destination = i - 1
+    if i == 0:
+        destination = len(qtile.screens) - 1
+    _window_to_screen(qtile, destination, switch_group, switch_screen)
 
 
 def window_to_next_screen(qtile, switch_group=False, switch_screen=False):
     i = qtile.screens.index(qtile.current_screen)
-    if i + 1 != len(qtile.screens):
-        group = qtile.screens[i + 1].group.name
-        qtile.current_window.togroup(group, switch_group=switch_group)
-        if switch_screen:
-            qtile.cmd_to_screen(i + 1)
+    destination = i + 1
+    if i == len(qtile.screens) - 1:
+        destination = 0
+    _window_to_screen(qtile, destination, switch_group, switch_screen)
 
 
 def init_keybindings(groups):
@@ -79,12 +94,12 @@ def init_keybindings(groups):
         Key([META, SHIFT], "f", lazy.window.toggle_fullscreen()),
         Key(  # Move window between screens
             [META, SHIFT],
-            RIGHT,
+            END,
             lazy.function(window_to_next_screen, switch_screen=True),
         ),
         Key(  # Move window between screens
             [META, SHIFT],
-            LEFT,
+            HOME,
             lazy.function(window_to_previous_screen, switch_screen=True),
         ),
         # Meta + Ctrl + key
