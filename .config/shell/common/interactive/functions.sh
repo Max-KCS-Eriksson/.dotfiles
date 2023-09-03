83a7114f6f7b46c68764ccf3ef21452f991d604d
Max <teratoscincus@hotmail.com>
2023-09-03 20:37:12 +0200

    Shell: Add function to `cd` into project root dir

--- .config/shell/common/interactive/functions.sh
+++ .config/shell/common/interactive/functions.sh
@@ -20,3 +20,51 @@ loadcompletion() {
         ;;
     esac
 }
+
+# Get directory of given filename upwards from cwd
+findup() {
+    path_name=$PWD
+    while [[ "$path_name" != "" && ! -e "$path_name/$1" ]]; do
+        path_name=${path_name%/*}
+    done
+    echo "$path_name"
+}
+
+# `cd` into project root dir
+cdr() {
+    # Files indicating dir as project root dir
+    anchor_files=(
+        .bzr
+        .citc
+        .git
+        .hg
+        .node-version
+        .python-version
+        .go-version
+        .ruby-version
+        .lua-version
+        .java-version
+        .perl-version
+        .php-version
+        .tool-version
+        .shorten_folder_marker
+        .svn
+        .terraform
+        CVS
+        Cargo.toml
+        composer.json
+        go.mod
+        package.json
+        stack.yaml
+        Pipfile
+        pom.xml
+        settings.gradle
+    )
+
+    for anchor in "${anchor_files[@]}"; do
+        anchor_dir=$(findup "$anchor")
+        if [[ $anchor_dir != '' ]]; then
+            cd "$anchor_dir" || exit
+        fi
+    done
+}
